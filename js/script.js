41ba6648d0fe3a5fb545a3f8c023f1c132ef62f7
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-05-30 14:46:42 +0200

    tentativo di mostrare Fizz e Buzz

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 console.log('JS OK');
 
+//mostro in console i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
     console.log(i);
+
+    if (i % 3 == 0) {
+      console.log(i = 'Fizz');  
+    }
+    
+    if (i % 5 == 0) {
+        console.log(i = 'Buzz');  
+      }
 }
