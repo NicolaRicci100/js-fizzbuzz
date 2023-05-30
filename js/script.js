6db785f1ec0d6b94b662e58bc2d315d881fd770a
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-05-30 15:09:12 +0200

    aggiungo commenti sugli if

--- js/script.js
+++ js/script.js
@@ -4,11 +4,12 @@ console.log('JS OK');
 for (let i = 1; i <= 100; i++) {
     console.log(i);
 
-    if (i % 3 == 0) {
-      console.log(i = 'Fizz');  
+    //controllo se il numero mi da resto 0 se diviso per 3
+    if (i % 3 == 0) { 
+        console.log(i = 'Fizz');  
     }
-    
+    //controllo se il numero mi da resto 0 se diviso per 5
     if (i % 5 == 0) {
         console.log(i = 'Buzz');  
-      }
+    }
 }
