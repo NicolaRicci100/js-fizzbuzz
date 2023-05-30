653646e9292fb0f7a605ad53d6a08244f959f29c
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-05-30 16:59:22 +0200

    aggiustamento if else

--- js/script.js
+++ js/script.js
@@ -3,13 +3,15 @@ console.log('JS OK');
 //mostro in console i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
     
-
+    if (i % 3 == 0 && i % 5 == 0){ 
+        console.log('FizzBuzz');
+    }
     //controllo se il numero mi da resto 0 se diviso per 3
-    if (i % 3 == 0) { 
+    else if (i % 3 == 0) { 
         console.log('Fizz');
     }
     //controllo se il numero mi da resto 0 se diviso per 5
-    if (i % 5 == 0) {
+    else if (i % 5 == 0) {
         console.log('Buzz');  
     }
 
