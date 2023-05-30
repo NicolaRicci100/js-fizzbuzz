a1aa0d4e2718cc40b643b8d41f979b1346317998
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-05-30 15:36:59 +0200

    risistemo la formula

--- js/script.js
+++ js/script.js
@@ -4,12 +4,12 @@ console.log('JS OK');
 for (let i = 1; i <= 100; i++) {
     console.log(i);
 
-    //controllo se il numero è diverso da 3 e mi da resto 0 se diviso per 3
-    if (i != 3 && i % 3 == 0) { 
+    //controllo se il numero mi da resto 0 se diviso per 3
+    if (i % 3 == 0) { 
         console.log(i = 'Fizz');  
     }
-    //controllo se il numero è diverso da 5 e mi da resto 0 se diviso per 5
-    if (i != 5 && i % 5 == 0) {
+    //controllo se il numero mi da resto 0 se diviso per 5
+    if (i % 5 == 0) {
         console.log(i = 'Buzz');  
     }
 }
--- steps.md
+++ steps.md
@@ -6,9 +6,9 @@ Scrivi un programma che stampi in console i numeri da 1 a 100, ma che:
 
 # Steps
 - mostra in console numeri che vadano da 1 fino a 100 inclusi
-- **SE** il numero è diverso da 3 e mi da resto 0 se lo divido per 3
+- **SE** il numero è multiplo di 3 *(mi da resto 0 se lo divido per 3?)*
     - mostra *Fizz*
-- **SE** il numero è diverso da 5 e mi da resto 0 se lo divido per 5
+- **SE** il numero è multiplo di 5 *(mi da resto 0 se lo divido per 5?)*
     - mostra *Buzz*
 
 
