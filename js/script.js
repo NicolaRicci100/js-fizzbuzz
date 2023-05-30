dbe4200c6314760150aa7806168e9c5a711e824e
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-05-30 16:47:32 +0200

    aggiungo else

--- js/script.js
+++ js/script.js
@@ -2,14 +2,18 @@ console.log('JS OK');
 
 //mostro in console i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
+    
 
     //controllo se il numero mi da resto 0 se diviso per 3
     if (i % 3 == 0) { 
-        console.log('Fizz');  
+        console.log('Fizz');
     }
     //controllo se il numero mi da resto 0 se diviso per 5
     if (i % 5 == 0) {
         console.log('Buzz');  
+    }
+
+    else {
+        console.log(i);
     }
 }
