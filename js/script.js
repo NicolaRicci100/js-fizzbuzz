5e6a2125ba6c6bb98dd3bc94f547829eca10c9b7
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-05-30 17:10:52 +0200

    steps.md e commenti script.js aggiornati

--- js/script.js
+++ js/script.js
@@ -2,20 +2,21 @@ console.log('JS OK');
 
 //mostro in console i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
-    
-    if (i % 3 == 0 && i % 5 == 0){ 
-        console.log('FizzBuzz');
-    }
-    //controllo se il numero mi da resto 0 se diviso per 3
-    else if (i % 3 == 0) { 
-        console.log('Fizz');
-    }
-    //controllo se il numero mi da resto 0 se diviso per 5
-    else if (i % 5 == 0) {
-        console.log('Buzz');  
-    }
 
+    //controllo se il numero è multiplo sia di 3 che di 5
+    if (i % 3 == 0 && i % 5 == 0){ 
+        console.log('FizzBuzz');  //quindi mostro FizzBuzz
+    }
+    //controllo se il numero è solo multiplo di 3
+    else if (i % 3 == 0) { 
+        console.log('Fizz');  //quindi mostro Fizz
+    }
+    //controllo se il numero è solo multiplo di 5
+    else if (i % 5 == 0) {
+        console.log('Buzz');  //quindi mostro Buzz
+    }
+    
     else {
-        console.log(i);
+        console.log(i);  //altrimenti mostro solo il numero
     }
 }
--- steps.md
+++ steps.md
@@ -5,10 +5,14 @@ Scrivi un programma che stampi in console i numeri da 1 a 100, ma che:
 - Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero.
 
 # Steps
-- mostra in console numeri che vadano da 1 fino a 100 inclusi
-- **SE** il numero è multiplo di 3 *(mi da resto 0 se lo divido per 3?)*
-    - mostra *Fizz*
-- **SE** il numero è multiplo di 5 *(mi da resto 0 se lo divido per 5?)*
-    - mostra *Buzz*
+- Mostra in console numeri che vadano da 1 fino a 100 inclusi
+    - **SE** il numero è multiplo di 3 e di 5 *(mi da resto 0 se lo divido per 3 e 5)*
+        - mostra *FizzBuzz*
+    - **SE INVECE** il numero è solo multiplo di 3 *(mi da resto 0 se lo divido per 3)*
+        - mostra *Fizz*
+    - **SE INVECE** il numero è solo multiplo di 5 *(mi da resto 0 se lo divido per 5)*
+        - mostra *Buzz*
+    - **ALTRIMENTI**
+        - mostra solo il numero
 
 
